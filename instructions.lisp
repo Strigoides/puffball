cdd7abbe2df168135433cad69b390bf76f194ebf
Owen Rodley <Strigoides@gmail.com>
2013-01-26 23:12:38 +1300

    Direction changing instructions (<, v, ^, >), and test programs

--- instructions.lisp
+++ instructions.lisp
@@ -11,13 +11,14 @@
   "Defines a funge instruction corresponding to the character NAME.
    Instructions have one, implicit argument: IP, which is bound to the ip
    executing the instruction. The return value of the function is assigned to
-   the ip that executed it"
+   the ip that executed it, so destructively modifying the ip passed in is
+   perfectly acceptable"
   `(setf (gethash ,name *funge-98-instructions*)
          (lambda (ip)
            ,@body)))
 
 (define-funge-instruction #\@
-  "Kill the current ip"
+  "Kill the current IP"
   (declare (ignore ip))
   nil)
 
@@ -25,4 +26,28 @@
   "NOP"
   ;; TODO: Implement backtrack wrapping; " " shouldn't be a NOP, as it should
   ;; execute in "no time at all", as far as concurrency is concerned
+  ip)
+
+(define-funge-instruction #\<
+  "Change the DELTA of the IP to point to the left"
+  (setf (ip-delta ip)
+        #(-1 0))
+  ip)
+
+(define-funge-instruction #\v
+  "Change the DELTA of the IP to point down"
+  (setf (ip-delta ip)
+        #(0 1))
+  ip)
+
+(define-funge-instruction #\^
+  "Change the DELTA of the IP to point up"
+  (setf (ip-delta ip)
+        #(0 -1))
+  ip)
+
+(define-funge-instruction #\>
+  "Change the DELTA of the IP to point to the right"
+  (setf (ip-delta ip)
+        #(1 0))
   ip)
